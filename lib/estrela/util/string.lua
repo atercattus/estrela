7faa25556f9653ed6ff9096697f10c2db2c0ecd0
AterCattus <deatercattus@gmail.com>
2014-06-11 18:47:58 +0400

    добавил string.starts, string.ends и убрал лишние local переменные

--- lib/estrela/util/string.lua
+++ lib/estrela/util/string.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local _sub, _find, _gsub, _gfind, _gmatch = string.sub, string.find, string.gsub, string.gfind, string.gmatch
+local _find = string.find
 
 local T = require('estrela.util.table')
 
@@ -32,7 +32,7 @@ end
 
 function M.split(str, sep, maxsplit, sep_regex)
     if type(sep) ~= 'string' or #sep < 1 then
-        return T.list(_gmatch(str, '.'))
+        return T.list(str:gmatch('.'))
     end
 
     plain = not sep_regex
@@ -51,17 +51,17 @@ function M.split(str, sep, maxsplit, sep_regex)
     while #res < maxsplit - 1 do
         local pos, pos_t = _find(str, sep, prev_pos, plain)
         if not pos then
-            res[#res + 1] = _sub(str, prev_pos)
+            res[#res + 1] = str:sub(prev_pos)
             prev_pos = #str + 1
             break
         end
 
-        res[#res + 1] = _sub(str, prev_pos, pos - 1)
+        res[#res + 1] = str:sub(prev_pos, pos - 1)
         prev_pos = pos_t + 1
     end
 
     if prev_pos <= #str then
-        res[#res + 1] = _sub(str, prev_pos)
+        res[#res + 1] = str:sub(prev_pos)
     end
 
     return res
@@ -74,12 +74,12 @@ end
 
 function M.rtrim(str, chars)
     chars = chars or '%s'
-    return _gsub(str, '['..chars..']+$', '')
+    return str:gsub('['..chars..']+$', '')
 end
 
 function M.ltrim(str, chars)
     chars = chars or '%s'
-    return _gsub(str, '^['..chars..']+', '')
+    return str:gsub('^['..chars..']+', '')
 end
 
 function M.htmlencode(str, withQuotes)
@@ -97,10 +97,20 @@ function M.htmlencode(str, withQuotes)
     return str
 end
 
+function M.starts(str, prefix)
+    return prefix:len() == 0 or _find(str, prefix, 1, true) == 1
+end
+
+function M.ends(str, suffix)
+    local sl = suffix:len()
+    return sl == 0 or _find(str, suffix, -sl, true) == (str:len() - sl + 1)
+end
+
 function M.apply_patch()
     setmetatable(string, {
         __index = M,
     })
+    string.find = M.find
 end
 
 return M
