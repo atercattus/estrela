729373aa69d4805c881f272d9375b367840ccf41
AterCattus <deatercattus@gmail.com>
2014-06-13 22:38:36 +0400

    спрятал приватные методы ngx.app

--- lib/estrela/ngx/app.lua
+++ lib/estrela/ngx/app.lua
@@ -3,7 +3,54 @@ local OOP = require('estrela.oop.single')
 local Router = require('estrela.ngx.router')
 local Request = require('estrela.ngx.request')
 local Response = require('estrela.ngx.response')
-local PP = require('estrela.io.pprint').print
+
+local _app = {
+    _callErrorCb = function(self, errno)
+        local route = self.router:getByName(errno)
+        if route then
+            return self:_callRoute(route.cb, errno)
+        else
+            return ngx.exit(errno)
+        end
+    end,
+
+    _callDefers = function(self)
+        for _,defer in ipairs(self.defers) do
+            local ok, res = pcall(defer.cb, unpack(defer.args))
+            if not ok then
+                self.defers = {}
+                self.error = res
+                return self:_callErrorCb(ngx.HTTP_INTERNAL_SERVER_ERROR)
+            end
+        end
+        self.defers = {}
+    end,
+
+    _callRoute = function(self, cb, errno)
+        self.defers = {}
+
+        local ok, res = xpcall(
+            function()
+                return cb(self, self.req, self.resp)
+            end,
+            function(err)
+                return err .. '\n' .. debug.traceback()
+            end
+        )
+
+        self:_callDefers()
+
+        if ok then
+            return res
+        elseif errno == ngx.HTTP_INTERNAL_SERVER_ERROR then
+            -- ошибка в обработчике ошибки. прерываем работу
+            return ngx.exit(errno)
+        else
+            self.error = res
+            return self:_callErrorCb(ngx.HTTP_INTERNAL_SERVER_ERROR)
+        end
+    end,
+}
 
 return OOP.name 'ngx.app'.class {
     new = function(self, routes)
@@ -42,49 +89,7 @@ return OOP.name 'ngx.app'.class {
         return true
     end,
 
-    _callErrorCb = function(self, errno)
-        local route = self.router:getByName(errno)
-        if route then
-            return self:_callRoute(route.cb, errno)
-        else
-            return ngx.exit(errno)
-        end
-    end,
-
-    _callRoute = function(self, cb, errno)
-        self.defers = {}
-
-        local ok, res = xpcall(
-            function()
-                return cb(self, self.req, self.resp)
-            end,
-            function(err)
-                return err .. '\n' .. debug.traceback()
-            end
-        )
-
-        self:_callDefers()
-
-        if ok then
-            return res
-        elseif errno == ngx.HTTP_INTERNAL_SERVER_ERROR then
-            -- ошибка в обработчике ошибки. прерываем работу
-            return ngx.exit(errno)
-        else
-            self.error = res
-            return self:_callErrorCb(ngx.HTTP_INTERNAL_SERVER_ERROR)
-        end
-    end,
-
-    _callDefers = function(self)
-        for _,defer in ipairs(self.defers) do
-            local ok, res = pcall(defer.cb, unpack(defer.args))
-            if not ok then
-                self.defers = {}
-                self.error = res
-                return self:_callErrorCb(ngx.HTTP_INTERNAL_SERVER_ERROR)
-            end
-        end
-        self.defers = {}
+    __index__ = function(self, key)
+        return _app[key]
     end,
 }
