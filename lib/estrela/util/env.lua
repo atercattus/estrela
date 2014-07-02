9e593a21649e6e13dfc177b10748e80b5ad01a41
AterCattus <deatercattus@gmail.com>
2014-07-02 18:07:47 +0400

    доработка util.env

--- lib/estrela/util/env.lua
+++ lib/estrela/util/env.lua
@@ -2,8 +2,11 @@ local debug = require('debug')
 local debug_getinfo = debug.getinfo
 local string_len = string.len
 
+local S = require('estrela.util.string')
+local S_ltrim = S.ltrim
+local S_rtrim = S.rtrim
+
 local path_split = require('estrela.util.path').split
-local S_rtrim = require('estrela.util.string').rtrim
 
 local M = {}
 
@@ -14,7 +17,7 @@ function M.get_root()
 end
 
 local function _setup_env()
-    local _path = debug_getinfo(1).source:sub(2)
+    local _path = S_ltrim(debug_getinfo(1).source, '@')
     _path = S_rtrim(path_split(_path).path, [[/]])
     _lib_root = _path:sub(1, -string_len('/util')-1)
 end
